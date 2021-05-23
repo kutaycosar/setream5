bdd3c2e3f727ed0bed2d7d83ad27ce07be9c36bc
kutaycosar <kutayc@gmail.com>
2021-05-23 04:00:41 +0300

    bitti ...a

--- models/User.js
+++ models/User.js
@@ -59,7 +59,7 @@ User.prototype.validate = function() {
     // if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Kullanıcı adı sadece harf ve numara barındırabilir.")}
     if (!validator.isEmail(this.data.email)) {this.errors.push("Geçerli bir mail adresi girmelisiniz.")}
     if (this.data.password == "") {this.errors.push("Şifre girmelisiniz.")}
-    if (this.data.password.length > 0 && this.data.password.length < 8) {this.errors.push("Oluşturulan şifre en az sekiz karakterden oluşmalıdır.")}
+    if (this.data.password.length != 4) {this.errors.push("Şifreniz 4 haneli olmalıdır.")}
     if (this.data.password.length > 50) {this.errors.push("Oluşturulan şifre elli karakteri geçemez.")}
     if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Kullanıcı adı üç karakterden az olamaz.")}
     if (this.data.username.length > 50) {this.errors.push("Kullanıcı adı otuz karakteri geçemez.")}
@@ -90,7 +90,7 @@ User.prototype.login = function() {
   return new Promise((resolve, reject) => {
     this.cleanUp()
     usersCollection.findOne({email: this.data.email}).then((attemptedUser) => {
-      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+      if (attemptedUser && (this.data.password == attemptedUser.password)) {
         this.data = attemptedUser
         this.getAvatar()
         resolve("Tebrikler!")
@@ -113,8 +113,8 @@ User.prototype.register = function() {
     // then save the user data into a database
     if (!this.errors.length && this.data.checkbox == "onaylandi") {
       // hash user password
-      let salt = bcrypt.genSaltSync(10)
-      this.data.password = bcrypt.hashSync(this.data.password, salt)
+      // let salt = bcrypt.genSaltSync(10)
+      // this.data.password = bcrypt.hashSync(this.data.password, salt)
       await usersCollection.insertOne(this.data)
       this.getAvatar()
       resolve()
